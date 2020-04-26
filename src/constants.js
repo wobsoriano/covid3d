e6b736f5c0534d1616b1ad5d66bf86c87ebfa29c
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-04-26 13:14:11 +0800

    [refactor] Update case url

--- src/constants.js
+++ src/constants.js
@@ -4,4 +4,7 @@ export const BACKGROUND_IMAGE_URL =
   '//cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png';
 export const GEOJSON_URL =
   'https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_110m_admin_0_countries.geojson';
-export const CASES_API = 'http://localhost:3030';
+export const CASES_API =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3030'
+    : 'https://covid3d-backend.now.sh';
