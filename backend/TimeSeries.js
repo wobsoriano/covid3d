8d202ca0f15c98dc398b1c21f8bc954c81290cfb
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-10-04 09:56:23 +0800

    add comment for parellel request

--- backend/TimeSeries.js
+++ backend/TimeSeries.js
@@ -20,6 +20,7 @@ class TimeSeries {
 
     let countryMapper = {};
 
+    // Run in parallel
     const [confirmedRows, recoveredRows, deathRows] = await Promise.all([
       this.getConfirmedCases(),
       this.getRecovered(),
