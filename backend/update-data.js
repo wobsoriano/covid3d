31f2ae748308ce7d14914ebf517e2bf97c4e88be
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-10-04 13:47:51 +0800

    fix repo owner

--- backend/update-data.js
+++ backend/update-data.js
@@ -53,13 +53,12 @@ async function createCommit(octokit, { owner, repo, base, changes }) {
 }
 
 async function run() {
-  const octokit = github.getOctokit(process.env.PROFILE_TOKEN);
-  const { data: user } = await octokit.users.getAuthenticated();
+  const octokit = github.getOctokit('876085e54458a8015a9a11825ac1a2b0f6a411d4');
 
   const data = await TimeSeries.fetchTimeSeries();
 
   createCommit(octokit, {
-    owner: user.login,
+    owner: 'wobsoriano',
     repo: 'covid3d',
     changes: {
       commit: 'Update cases data',
