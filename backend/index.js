877a56fea753191a28842e1627615380dea460f5
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-10-04 10:56:39 +0800

    fix local data path

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const apicache = require('apicache');
 const cors = require('cors');
+const path = require('path');
 const fs = require('fs');
 
 const cache = apicache.middleware;
@@ -12,7 +13,8 @@ app.use(cors());
 app.use(cache('1 hour'));
 
 app.get('/', (req, res) => {
-  fs.readFile('data.json', (err, buffer) => {
+  const location = path.join(__dirname, 'data.json');
+  fs.readFile(location, (err, buffer) => {
     if (err) {
       console.log(err);
       return res.status(500).send({ err });
